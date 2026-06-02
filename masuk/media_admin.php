0ed47cb2ef98a6a70cb4a41777dbbe7c2c9021a1
Ahmad Nurdiansyah <ahmadnurdiansyah81@gmail.com>
2026-06-02 07:31:58 +0700

    mengganti simbol glyphicon untuk ujian

--- masuk/media_admin.php
+++ masuk/media_admin.php
@@ -344,7 +344,7 @@ if ($_SESSION['login'] == 0) {
 									<?php if ($_SESSION['mjenisobat'] == "Y") { ?><li><a href="?module=jenisobat"><i class="glyphicon glyphicon-tags"></i> Jenis Obat & Rak Obat</a></li><?php } ?>
 									<?php if ($_SESSION['mbarang'] == "Y") { ?><li><a href="?module=barang"><i class="glyphicon glyphicon-book"></i> Item Barang</a></li><?php } ?>
 									<?php if ($_SESSION['komisi'] == "Y") { ?><li><a href="?module=komisi"><i class="glyphicon glyphicon-usd"></i> Komisi Pegawai</a></li><?php } ?>
-									<?php if ($canAccessUjian) { ?><li><a href="?module=ujian"><i class="glyphicon glyphicon-usd"></i> Ujian</a></li><?php } ?>
+									<?php if ($canAccessUjian) { ?><li><a href="?module=ujian"><i class="glyphicon glyphicon-list-alt"></i> Ujian</a></li><?php } ?>
 								</ul>
 							</li>
 							<li class="treeview">
