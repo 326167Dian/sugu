4dfe935886c220453ecea164ea83c655f8964e0e
ahmad nurdiansyah <ahmadnurdiansyah81@gmail.com>
2026-08-28 01:00:24 +0700

    tombol export to excel stok kritis sesuai tampilan

--- masuk/modul/mod_laporan/cetak_stokkritis.php
+++ masuk/modul/mod_laporan/cetak_stokkritis.php
@@ -21,43 +21,55 @@ echo $tgl_awal; ?>
 <table border="1">
     <tr>
         <th>No</th>
-        <th>Kode Barang</th>
+        <th>Kategori</th>
         <th>Nama Barang</th>
-        <th>Stok Barang</th>
+        <th>Qty/Stok</th>
+        <th>T30</th>
+        <th>Q30</th>
+        <th>SFC max30</th>
+        <th>SFCmax/week</th>
         <th>Satuan</th>
-        <th>t30</th>
-        <th>q30</th>
-        
-        <th>Harga Beli</th>
-        
     </tr>
     <?php
-    // koneksi database
-    include "../../../configurasi/koneksi.php";
-    // menampilkan data barang
-    $data = $db->query("select * from barang where t30>0 order by t30 desc ");
+    $hasil30 = $db->prepare("select id_barang,
+                                        kd_barang,
+                                        nm_barang,
+                                        stok_barang,
+                                        sat_barang,
+                                        t30,
+                                        q30
+                                 from barang ");
+    $hasil30->execute();
     $no = 1;
-    while ($d = $data->fetch(PDO::FETCH_ASSOC)) {
-    $angka = intval($d['kd_barang']);
+    while ($tp30 = $hasil30->fetch(PDO::FETCH_ASSOC)) {
+        $qfc = $tp30['t30'] - $tp30['stok_barang'];
+        $qweek = round((($tp30['q30'] - $tp30['stok_barang']) / 4), 0);
+
+        if ($tp30['t30'] > 0 && ($tp30['stok_barang'] <= (0.25 * $tp30['t30']))) {
+            if ($tp30['t30'] <= 0) {
+                $kategori = 'MACET';
+            } elseif ($tp30['t30'] > 0 && $tp30['t30'] <= 5) {
+                $kategori = 'SLOW';
+            } elseif ($tp30['t30'] > 5 && $tp30['t30'] <= 10) {
+                $kategori = 'LANCAR';
+            } else {
+                $kategori = 'LAKU';
+            }
     ?>
         <tr>
             <td style='text-align:center;'><?php echo $no; ?></td>
-            <td><?php echo $angka; ?></td>
-            <td><?php echo $d['nm_barang']; ?></td>
-            <td style='text-align:center;'><?php echo $d['stok_barang']; ?></td>
-            <td style='text-align:center;'><?php echo $d['sat_barang']; ?></td>
-            <td style='text-align:center;'><?php echo $d['t30']; ?></td>
-            <td style='text-align:center;'><?php echo $d['q30']; ?></td>
-            <td><?php echo $d['hrgsat_barang']; ?></td>
-            
+            <td style='text-align:center;'><?php echo $kategori; ?></td>
+            <td><?php echo $tp30['nm_barang']; ?></td>
+            <td style='text-align:center;'><?php echo $tp30['stok_barang']; ?></td>
+            <td style='text-align:center;'><?php echo $tp30['t30']; ?></td>
+            <td style='text-align:center;'><?php echo $tp30['q30']; ?></td>
+            <td style='text-align:center;'><?php echo $qfc; ?></td>
+            <td style='text-align:center;'><?php echo $qweek; ?></td>
+            <td style='text-align:center;'><?php echo $tp30['sat_barang']; ?></td>
         </tr>
-        
     <?php
-    $no++;
+        $no++;
+        }
     }
     ?>
 </table>
-
-
-
-?>
